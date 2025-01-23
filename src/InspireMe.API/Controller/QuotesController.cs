d4d00bd66a2f245f3f16a483d92a7fbff0d80f53
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-23 02:54:02 +0000

    refactor: ajusta QuotesController para usar injeção de dependência

--- src/InspireMe.API/Controller/QuotesController.cs
+++ src/InspireMe.API/Controller/QuotesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using InspireMe.API.Models;
+using InspireMe.API.Services;
 
 namespace InspireMe.API.Controllers
 {
@@ -7,31 +8,28 @@ namespace InspireMe.API.Controllers
     [Route("api/[controller]")]
     public class QuotesController : ControllerBase
     {
-        private static readonly List<Quote> Quotes = new List<Quote>
+        private readonly IQuotesService _quotesService;
+
+        public QuotesController(IQuotesService quotesService)
         {
-            new Quote { Id = 1, Text = "O melhor jeito de prever o futuro é criá-lo.", Author = "Peter Drucker" },
-            new Quote { Id = 2, Text = "A vida é o que acontece enquanto você está ocupado fazendo outros planos.", Author = "John Lennon" },
-            new Quote { Id = 3, Text = "Seja a mudança que você deseja ver no mundo.", Author = "Mahatma Gandhi" },
-            new Quote { Id = 4, Text = "Acredite que você pode, e você já está no meio do caminho.", Author = "Theodore Roosevelt" },
-            new Quote { Id = 5, Text = "A única maneira de fazer um excelente trabalho é amar o que você faz.", Author = "Steve Jobs" }
-        };
+            _quotesService = quotesService;
+        }
 
         [HttpGet]
-        public IActionResult Get() => Ok(Quotes);
+        public IActionResult Get() => Ok(_quotesService.GetAllQuotes());
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            var quote = Quotes.FirstOrDefault(q => q.Id == id);
+            var quote = _quotesService.GetQuoteById(id);
             return quote != null ? Ok(quote) : NotFound();
         }
 
         [HttpPost]
         public IActionResult Add(Quote newQuote)
         {
-            newQuote.Id = Quotes.Count + 1;
-            Quotes.Add(newQuote);
-            return CreatedAtAction(nameof(GetById), new { id = newQuote.Id }, newQuote);
+            var addedQuote = _quotesService.AddQuote(newQuote);
+            return CreatedAtAction(nameof(GetById), new { id = addedQuote.Id }, addedQuote);
         }
     }
 }
