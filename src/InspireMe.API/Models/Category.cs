49dc85778cb6283c344bc026d9414bcb11cc251b
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-23 03:56:54 +0000

    fix: ajusta nullabilidade nas propriedades do modelo Category

--- src/InspireMe.API/Models/Category.cs
+++ src/InspireMe.API/Models/Category.cs
@@ -1,6 +1,9 @@
-public class Category
+namespace InspireMe.API.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty; 
+        public string Description { get; set; } = string.Empty; 
+    }
 }
